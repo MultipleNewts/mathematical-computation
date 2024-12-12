aae39c9935fc67c0dd0d9c7881c067a0e4975211
Matt <123matt.hew654@gmail.com>
2024-12-12 11:56:41 +0000

    Add various simple funcs
    Added computational methods of solving integer powers, square roots, and used the CORDIC algorithm to compute sine and cosine

---
+++ algorithms.py
@@ -0,0 +1,61 @@
+# %%
+# this is termporarily being used in place of a lookup table
+from math import atan2
+
+pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342
+
+# Method for exponentiation: iteratively multiply to obtain value
+def exponent(base, power):
+    total = 1
+    for i in range(abs(power)):
+        total *= base
+    if power < 0:
+        total = 1/total
+    return total
+
+
+# Method for square roots: uses an iterative method known as Heron's Method
+def SqRoot_Heron(base, n=100):
+    x = 1
+    for i in range(n):
+        x = 0.5*(x+(base/x))
+    return x
+
+
+# Method for various function: finds side lengths of a traingle within a unit circle
+def CORDIC_rotation(angle, n=40):
+    v_cur = [0, 1]
+    K = 1
+    sigma = 1
+    for i in range(n):
+        if (angle-atan2(v_cur[1], v_cur[0])) > 0:
+            sigma = 1
+        else:
+            sigma = -1
+        K *= 1/SqRoot_Heron(1+exponent(2, (-2*i)))
+        x2 = v_cur[0]+(-sigma*exponent(2, (-i)))*v_cur[1]
+        y2 = (sigma*exponent(2, (-i))*v_cur[0])+v_cur[1]
+        v_cur = [x2, y2]
+    v_cur = [K*v_cur[0], K*v_cur[1]]
+    return v_cur
+
+
+def cos(angle):
+    return CORDIC_rotation(angle)[0]
+
+
+def sin(angle):
+    return CORDIC_rotation(angle)[1]
+
+
+def sin_deg(degrees):
+    return sin(degrees * pi/180)
+
+
+def cos_deg(degrees):
+    return cos(degrees * pi/180)
+
+
+print(sin(0.48487))
+
+# %%
